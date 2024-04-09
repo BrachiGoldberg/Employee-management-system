ed3728361f2a6c518e1b87173f8a6880e7146d08
Brachi <brahcig404@gmail.com>
2024-04-09 23:36:28 +0300

    add the can activate also to companys' components. in addition change the visible of date with DatePipe

--- employee-management-client/src/app/modules/company/company-routing.module.ts
+++ employee-management-client/src/app/modules/company/company-routing.module.ts
@@ -6,16 +6,17 @@ import { CompanyTermsComponent } from "./company-terms/company-terms.component";
 import { CompanyDetailsComponent } from "./company-details/company-details.component";
 import { UpdateCompanyDetailsComponent } from "./update-company-details/update-company-details.component";
 import { UpdateCompanyEntryDetailsComponent } from "./update-company-entry-details/update-company-entry-details.component";
+import { permissionNavigateGuard } from "../../permission-navigate.guard";
 
 const companyRoutes: Route[] = [
     { path: "", redirectTo: "login-company", pathMatch: 'full' },
     { path: "login-company", component: LoginToCompanyComponent },
     { path: "new-company", component: AddNewCompanyComponent },
-    { path: "update-company/:id", component: UpdateCompanyDetailsComponent },
+    { path: "update-company/:id", component: UpdateCompanyDetailsComponent, canActivate: [permissionNavigateGuard] },
     { path: "new-terms", component: CompanyTermsComponent },
-    { path: "comp-id/:compId/update-terms/:id", component: CompanyTermsComponent },
-    { path: "details/:id", component: CompanyDetailsComponent },
-    {path: "comp-id/:id/update-entry-details", component: UpdateCompanyEntryDetailsComponent}
+    { path: "comp-id/:compId/update-terms/:id", component: CompanyTermsComponent, canActivate: [permissionNavigateGuard] },
+    { path: "details/:id", component: CompanyDetailsComponent, canActivate: [permissionNavigateGuard] },
+    {path: "comp-id/:id/update-entry-details", component: UpdateCompanyEntryDetailsComponent, canActivate: [permissionNavigateGuard]}
 ]
 
 
